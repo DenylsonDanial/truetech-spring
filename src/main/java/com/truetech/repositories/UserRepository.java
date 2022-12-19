1cebbe4cb2ebdc352c59934f8320580ea052d85e
Denylson Danial <denylsondanial@gmail.com>
2022-12-19 12:40:37 +0200

    Nova actualização

--- src/main/java/com/truetech/repositories/UserRepository.java
+++ src/main/java/com/truetech/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.truetech.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.truetech.entities.*;
+import com.truetech.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
---
+++ src/main/java/com/truetech/services/UserService.java
@@ -0,0 +1,9 @@
+package com.truetech.services;
+
+public class UserService {
+
+	public UserService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
